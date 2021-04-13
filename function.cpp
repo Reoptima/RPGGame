1c58663de4c379e7bca6aa683fdbc65dd230c8d6
Андрей <sr.andrey.rodionov@gmail.com>
2021-04-13 15:28:51 +0300

    bugfix

--- function.cpp
+++ function.cpp
@@ -2,7 +2,6 @@
 #include <windows.h>
 #include <string>
 #include <list>
-
 using namespace std;
 
 
@@ -26,7 +25,8 @@ void clear() {
     system("cls");
 //    string indent = "\n";
 //    cout << indent * 40;
-};
+}
+
 bool is_number(const std::string& s)
 {
     std::string::const_iterator it = s.begin();
@@ -34,10 +34,11 @@ bool is_number(const std::string& s)
     return !s.empty() && it == s.end();
 }
 
-int input_int(const string &name) {
-    int i;
+int input_int(const string& name)
+{
     string a;
-    cout << "Сделайте выбор " + name + ": ";
+    int i;
+    cout << "Сделайте выбор: "+name+": ";
     while (true){
         getline(cin, a);
         if (is_number(a)){
@@ -46,51 +47,53 @@ int input_int(const string &name) {
     }
 }
 
-string input_str(const string &name) {
+string input_str(const string& name)
+{
     string a;
-    cout << "Добро пожаловать в MPT_CPP_MEGA_WOW_DELUXE-EDITION\nВведите " <<name << ": ";
-    cin >> a;
-    getline(cin,a);
+    cout << "Добро пожаловать в MPT_CPP_MEGA_WOW_DELUXE-EDITION \nВведите "+name+": ";
+    getline(cin, a);
     return a;
 }
 
-enum ConsoleColor {
-    Black = 0,
-    Blue = 1,
-    Green = 2,
-    Cyan = 3,
-    Red = 4,
-    Magenta = 5,
-    Brown = 6,
-    LightGray = 7,
-    DarkGray = 8,
-    LightBlue = 9,
-    LightGreen = 10,
-    LightCyan = 11,
-    LightRed = 12,
-    LightMagenta = 13,
-    Yellow = 14,
-    White = 15
+enum ConsoleColor
+{
+    Black         = 0,
+    Blue          = 1,
+    Green         = 2,
+    Cyan          = 3,
+    Red           = 4,
+    Magenta       = 5,
+    Brown         = 6,
+    LightGray     = 7,
+    DarkGray      = 8,
+    LightBlue     = 9,
+    LightGreen    = 10,
+    LightCyan     = 11,
+    LightRed      = 12,
+    LightMagenta  = 13,
+    Yellow        = 14,
+    White         = 15
 };
 
-void SetColor(ConsoleColor text, ConsoleColor background) {
+void SetColor(ConsoleColor text, ConsoleColor background)
+{
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, (WORD) ((background << 4) | text));
+    SetConsoleTextAttribute(hConsole, (WORD)((background << 4) | text));
 }
 
-int choice(const string &question, const list<string> &variants) {
+int choice(const string& question, const list<string>& variants){
     std::cout << question << std::endl;
     int number = 1;
     SetColor(White, Black);
-    for (const auto &variant : variants) {
-        std::cout << to_string(number) + " - " + variant << std::endl;
-        number += 1;
+    for (const auto& variant : variants){
+        std::cout << to_string(number)+" - "+variant << std::endl;
+        number+=1;
     }
     SetColor(White, Black);
     return input_int("");
 }
 
-int choiceWhile(const string &question, const list<string> &variants, bool IsZero = false) {
+int choiceWhile(const string& question, const list<string>& variants, bool IsZero=false){
     while (true) {
         clear();
         std::cout << question << std::endl;
@@ -102,21 +105,30 @@ int choiceWhile(const string &question, const list<string> &variants, bool IsZer
         }
         SetColor(White, Black);
         int result = input_int("");
-        if (((IsZero and result > -1) or result > 0) and result < variants.size() + 1) {
+        if (((IsZero and result>-1) or result>0) and result<variants.size()+1){
             return result;
         }
     }
 }
 
-bool percentageChance(int percentage) {
-    return percentage > rand() % 100;
+bool percentageChance(int percentage){
+    return percentage>rand()%100;
 }
 
-int randInt(int from, int before) {
-    int number = rand() % (before + 1);
-    if (number < from) {
-        number += from;
+int randInt(int from, int before){
+    int number = rand()%(before+1);
+    if(number<from){
+        number+=from;
     }
     return number;
 }
 
+bool sortLevel(vector<string> i1, vector<string> i2)
+{
+    return stoi(i1[1])>stoi(i2[1]);
+}
+
+bool sortGold(vector<string> i1, vector<string> i2)
+{
+    return stoi(i1[2])>stoi(i2[2]);
+}
